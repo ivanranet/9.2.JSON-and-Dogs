1353f3b6a37440cefaa51813ae0ff57cd0ac1aba
ivanranet <ivanranet@yandex.ru>
2024-11-16 15:46:57 +0500

    создал функцию загрузки изображения, метку и кнопку

--- JSON and dogs.py
+++ JSON and dogs.py
@@ -1,7 +1,25 @@
 from tkinter import *
 import requests
+from tkinter import messagebox as mb
 from PIL import Image, ImageTk
 from io import BytesIO
+
+from bottle import response
+from pygame.examples.cursors import image
+
+def show_image():
+    image_url = get_dog_image()
+    if image_url:
+        try:
+            response = requests.get(image_url, stream = True)
+            response.raise_for_status()
+            img_data = BytesIO(response.content)
+            img = Image.open(img_data)
+            img.thumbnail((300, 300))
+            label.config(image = img)
+            label.image = img
+        except Exception as e:
+            mb.showerror('Ошибка!', f'Возникла ошибка: {e}')
 
 window = Tk()
 window.title('Картинки с "собатшками"')
