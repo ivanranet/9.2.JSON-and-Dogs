9faca42faefbdd0c854c26e0271ccd2cf47ff2ba
ivanranet <ivanranet@yandex.ru>
2024-11-16 16:37:20 +0500

    создал функцию загрузки ссылок на изображения

--- JSON and dogs.py
+++ JSON and dogs.py
@@ -4,8 +4,16 @@ from tkinter import messagebox as mb
 from PIL import Image, ImageTk
 from io import BytesIO
 
-from bottle import response
-from pygame.examples.cursors import image
+
+def get_dog_image():
+    try:
+        response = requests.get('https://dog.ceo/api/breeds/image/random')
+        response.raise_for_status()
+        data = response.json()
+        return  data('message')
+    except Exception as e:
+        mb.showerror('Ошибка!', f'Ошибка при запросе к API: {e}')
+
 
 def show_image():
     image_url = get_dog_image()
@@ -16,10 +24,12 @@ def show_image():
             img_data = BytesIO(response.content)
             img = Image.open(img_data)
             img.thumbnail((300, 300))
+            img = ImageTk.PhotoImage(img)
             label.config(image = img)
             label.image = img
         except Exception as e:
-            mb.showerror('Ошибка!', f'Возникла ошибка: {e}')
+            mb.showerror('Ошибка!', f'Ошибка загрузки изображения: {e}')
+
 
 window = Tk()
 window.title('Картинки с "собатшками"')
