df3ccd2157e8f3f1cbe2e6dc0b3b616fa86e94a3
ivanranet <ivanranet@yandex.ru>
2024-11-16 15:21:33 +0500

    создал главное окно

--- JSON and dogs.py
+++ JSON and dogs.py
@@ -1,1 +1,16 @@
 from tkinter import *
+import requests
+from PIL import Image, ImageTk
+from io import BytesIO
+
+window = Tk()
+window.title('Картинки с "собатшками"')
+window.geometry('360x420')
+
+label = Label()
+label.pack(pady = 10)
+
+button = Button(text = 'Загрузить изображение', command = show_image)
+button.pack(pady = 10)
+
+window.mainloop()
